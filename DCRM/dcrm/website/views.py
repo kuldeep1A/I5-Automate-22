34dc92264d155bf4db00be65954afdf053de836f
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-12 22:39:21 +0530

    complete the 'register'

--- DCRM/dcrm/website/views.py
+++ DCRM/dcrm/website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django import forms
+from .forms import SignupForm
 
 
 def home(request):
@@ -26,6 +28,20 @@ def logout_user(request):
     return redirect('home')
 
 
-def register(request):
-    return render(request, 'register.html')
+def register_user(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignupForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
 
